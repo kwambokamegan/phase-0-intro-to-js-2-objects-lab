427869931ec86026a6d830c3244ce28daf0f6fe7
Megan <megan.nyakina@student.moringaschool.com>
2024-06-27 14:52:10 +0300

    adds intro to js objects lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution in this file!
+let employee ={
+    name :"Sam",
+     streetAdress :"11 Broadway",
+
+}
+
+
+
+//updateEmployeeWithKeyAndValue
+
+function updateEmployeeWithKeyAndValue(employee , key , value)
+
+{ return {...employee, [key]: value } }
+
+
+ 
+//destructivelyUpdateEmployeeWthKeyAndValue
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee , key , value){
+     employee[key] = value;
+    return employee ; 
+}
+
+  
+
+//deleteFromEmployeeByKey
+
+
+function deleteFromEmployeeByKey (employee , key)
+
+{
+   let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee ; 
+
+}
+
+
+//destructivelyDeleteFromEmployeeByKey 
+
+function destructivelyDeleteFromEmployeeByKey ( employee , key )
+{
+    delete employee[key];
+    return employee;
+}
+
+   
